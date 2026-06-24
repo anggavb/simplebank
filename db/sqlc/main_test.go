ded976da946026d0db37d7c1a5605b22cb52b2cd
anggavb <anggavb8@gmail.com>
2026-06-24 12:25:11 +0700

    feat(config): add configuration loading from environment variables using viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -4,18 +4,10 @@ import (
 	"context"
 	"log"
 	"os"
-	"strconv"
 	"testing"
 
+	"github.com/backend-grpc/util"
 	"github.com/jackc/pgx/v5/pgxpool"
-)
-
-const (
-	dbHost     = "localhost"
-	dbPort     = 5050
-	dbName     = "grpc_bank"
-	dbUser     = "grpc_bank"
-	dbPassword = "secret"
 )
 
 var testQueries *Queries
@@ -23,15 +15,15 @@ var testDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = pgxpool.New(context.Background(), buildDBURL())
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err.Error())
 	}
 
 	testQueries = New(testDB)
 	os.Exit(m.Run())
-}
-
-func buildDBURL() string {
-	return "postgres://" + dbUser + ":" + dbPassword + "@" + dbHost + ":" + strconv.Itoa(dbPort) + "/" + dbName + "?sslmode=disable"
 }
--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 
 	"github.com/backend-grpc/api"
 	db "github.com/backend-grpc/db/sqlc"
+	"github.com/backend-grpc/util"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Printf("Error loading env. \ncause: %s", err.Error())
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
 	}
 
-	conn, err := pgxpool.New(context.Background(), os.Getenv("DB_URL"))
+	conn, err := pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err.Error())
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err := server.Start(os.Getenv("SERVER_ADDRESS")); err != nil {
+	if err := server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start server:", err.Error())
 	}
 }
